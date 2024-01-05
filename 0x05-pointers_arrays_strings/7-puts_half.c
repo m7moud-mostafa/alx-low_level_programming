eef46060c02d6e19a079f5f303df642b3f118b73
Mahmoud <mah2002moud@gmail.com>
2024-01-05 18:08:09 +0200

    AlxProject

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int n;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		n = length / 2 - 1 ;
+	else
+		n = (length - 1) / 2;
+
+	for (; n < length; n++)
+		_putchar(str[n]);
+	_putchar('\n');
+
+}
