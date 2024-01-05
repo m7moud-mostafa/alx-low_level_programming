fb5e4577873824bf027b3f9286d7b2a777284954
Mahmoud <mah2002moud@gmail.com>
2024-01-05 18:43:15 +0200

    AlxProject

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies array
+ * @dest: copied array
+ * @src: array
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+	int i = 0;
+	while (src[length] != '\0')
+		length++;
+
+	for (; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[length] = '\0';
+
+	return (dest);
+}
