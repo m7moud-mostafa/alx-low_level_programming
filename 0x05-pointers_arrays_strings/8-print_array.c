3cc6531f9aad2161c3c464fc7435c4a34d3e8cd5
Mahmoud <mah2002moud@gmail.com>
2024-01-05 18:25:45 +0200

    AlxProject

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints array
+ * @a: array
+ * @n: number of elements to printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%i , ", a[i]);
+		else
+			printf("%i\n", a[i]);
+	}
+}
