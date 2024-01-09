10b50030f16b6d7ca9c55a0de48f281e14616398
Mahmoud <mah2002moud@gmail.com>
2024-01-09 04:36:31 +0200

    AlxProject

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be scanned.
+ * @accept: The string containing the set of bytes to match.
+ *
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				return s;
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
+}
