b70216774f3cb6243297677f8a63422c16c5a528
Mahmoud <mah2002moud@gmail.com>
2024-01-09 04:38:27 +0200

    AlxProject

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	if (*needle == '\0')
+	{
+		return haystack;
+	}
+
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+
+		while (*n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return haystack;
+		}
+
+		haystack++;
+	}
+
+	return (NULL);
+}
