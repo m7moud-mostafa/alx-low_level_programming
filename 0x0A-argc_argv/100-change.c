394e3c4c330aaae362456c8a2e4f354990475b27
Mahmoud <mah2002moud@gmail.com>
2024-01-13 04:46:22 +0200

    AlxProject

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function to print the name of the program
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int num_change = 0;
+	int change;
+
+	change = atoi(argv[1]);
+
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_change += change / 25;
+	change = change % 25;
+
+	num_change += change / 10;
+	change = change % 10;
+
+	num_change += change / 5;
+	change = change % 5;
+
+	num_change += change / 2;
+	change = change % 2;
+
+	num_change += change / 1;
+	printf("%i\n", num_change);
+
+	return (0);
+}
