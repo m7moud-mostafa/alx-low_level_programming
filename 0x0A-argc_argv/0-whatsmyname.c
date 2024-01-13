f4d097933cd027beaf453af9b46f802b58a2f38d
Mahmoud <mah2002moud@gmail.com>
2024-01-13 03:32:00 +0200

    AlxProject

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ /**
+  * main - main function to print the name of the program
+  * @argc: arg counter
+  * @argv: arg vector
+  * Return: 0 
+  */
+ int main(__attribute__((unused)) int argc, char *argv[])
+ {
+	printf("%s\n",argv[0]);
+	return (0);
+ }
