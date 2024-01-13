5dbc64c5c1adfca2fd476ea268983b1d62bf4fbd
Mahmoud <mah2002moud@gmail.com>
2024-01-13 03:57:39 +0200

    AlxProject

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function to print the name of the program
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	printf("%i\n", i * j);
+
+	return (0);
+}
