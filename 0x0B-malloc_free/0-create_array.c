7f77b32e17f4752eb25700c929a1b58a0ae01f1b
Mahmoud <mah2002moud@gmail.com>
2024-01-17 03:59:29 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,18 @@ char *create_array(unsigned int size, char c)
 	char *address;
 	unsigned int i;
 
+	if (size == NULL)
+		return (NULL);
+
 	address = malloc(size);
 
-	if (address != NULL && size > 0)
-	{
-		for (i = 0; i < size; i++)
-			address[i] = c;
-		address[size] = c;
-	}
+	if (address == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		address[i] = c;
+	address[size] = c;
+
 
 	return (address);
 }
