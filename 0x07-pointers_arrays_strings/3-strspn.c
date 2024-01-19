535e2948d652aafde5f6457eb9d9e540b6d845c3
Mahmoud <mah2002moud@gmail.com>
2024-01-19 02:52:49 +0200

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,5 +10,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int lens = 0;
+	unsigned int lena = 0;
+	unsigned int n = 0;
+	unsigned int i = 0;
+	unsigned int j;
 
+	while (s[lens] != '\0')
+		lens++;
+
+	while (accept[lena] != '\0')
+		lena++;
+
+	for (; i < lens && n == i; i++)
+	{
+		for (j = 0; j < lena; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				n++;
+				break;
+			}
+		}
+	}
+	return (n);
 }
