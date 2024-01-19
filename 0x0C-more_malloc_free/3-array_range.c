fed6e86720fa2d155a35c93c0d25b5276c016655
Mahmoud <mah2002moud@gmail.com>
2024-01-19 04:57:31 +0200

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,3 +8,21 @@
  *         NULL if min > max or if memory allocation fails.
  */
 int *array_range(int min, int max)
+{
+	int *array;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		array[i] = min;
+
+	return (array);
+}
