56ffea6260f71570e641b771c1715fb062da526c
Mahmoud <mah2002moud@gmail.com>
2024-01-26 21:34:39 +0200

    AlxProject

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initializing the variable of type struct dog
+ * @d:the address of the variable
+ * @name: name to be assigned
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
