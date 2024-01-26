8b3adabecd3f06b291f70fda45d76a78cb5cf562
Mahmoud <mah2002moud@gmail.com>
2024-01-26 23:41:49 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,35 @@
 #include "dog.h"
+
+/**
+ * _strdub - duplicates a string
+ * @s: source string
+ * Return: copy of s (dust)
+ */
+void *_strdub(char *s)
+{
+	char *dust;
+	int len;
+	int i;
+
+	len = 0;
+	if (s == NULL)
+		return (NULL);
+
+	while (s[len] != '\0')
+		len++;
+
+	dust = malloc(len + 1);
+
+	if (dust == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dust[i] = s[i];
+
+	return (dust);
+}
+
+
 /**
  * new_dog - initializing dog type variable
  * @name: name
@@ -11,12 +42,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	if (name == NULL || age <= 0 || owner == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
 
-	dog->name = name;
+	dog->name = _strdub(name);
+	if (dog->name == NULL)
+	{
+		free (dog);
+		return (NULL);
+	}
+	dog->owner = _strdub(owner);
+	if (dog->owner == NULL)
+	{
+		free (dog);
+		return (NULL);
+	}
+
 	dog->age = age;
-	dog->owner = owner;
+	if (dog->age <= 0)
+	{
+		free (dog);
+		return (NULL);
+	}
 
 	return (dog);
 }
