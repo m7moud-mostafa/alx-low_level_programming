ad4a0f6ce6a2548e482333268cc55345ff64db96
Mahmoud <mah2002moud@gmail.com>
2024-01-29 03:45:32 +0200

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
+
 
 /**
  * print_all - prints anything
@@ -9,40 +8,40 @@
 void print_all(const char * const format, ...)
 {
 	va_list var;
-	int len = 0;
+	int len = 0, need_comma = 0;
 	char sym;
 	char *string;
 
 	va_start(var, format);
-	while (format[len] != '\0')
+	while (format && format[len] != '\0')
 	{
 		sym = format[len];
-		switch (sym)
-		{
-		case 'c':
-			printf("%c", va_arg(var, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(var, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(var, double));
-			break;
-		case 's':
-				string = va_arg(var, char *);
-				if (!string)
-				{
-					string = "(nil)";
-				}
-				printf("%s", string);
-				break;
-		default:
-			len++;
-			continue;
-		}
-		if (format[len + 1] != '\0')
+		if (need_comma && (sym == 'c' || sym == 'i' || sym == 'f' || sym == 's'))
 		{
 			printf(", ");
+			need_comma = 0;
+		}
+		switch (sym)
+		{
+			case 'c':
+				printf("%c", va_arg(var, int));
+				need_comma = 1;
+				break;
+			case 'i':
+				printf("%i", va_arg(var, int));
+				need_comma = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(var, double));
+				need_comma = 1;
+				break;
+			case 's':
+				string = va_arg(var, char *);
+				if (!string)
+					string = "(nil)";
+				printf("%s", string);
+				need_comma = 1;
+				break;
 		}
 		len++;
 	}
