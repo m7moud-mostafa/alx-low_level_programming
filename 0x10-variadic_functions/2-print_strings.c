c8b0a05d2ac0dcf2cb58bd6194fbbdb90cd6267c
Mahmoud <mah2002moud@gmail.com>
2024-01-29 02:18:42 +0200

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,35 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-/**
- * _strdup - duplicates string
- * @str: string
- * Return: duplicated string
- *
- */
-
-char *_strdup(char *str)
-{
-	int len = 0;
-	int i;
-	char *dust;
-
-	if (str == NULL)
-		return (NULL);
-	while (str[len] != '\0')
-		len++;
-
-	dust = malloc(len + 1);
-	if (dust == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
-		dust[i] = str[i];
-
-	return (dust);
-}
 
 /**
  * print_strings - prints strings, followed by a new line
@@ -45,22 +16,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
-		string = _strdup(va_arg(strings, char *));
+		string = va_arg(strings, char *);
 
 		if (string == NULL)
 			printf("(nil)");
 
 		else
 			printf("%s", string);
-		free(string);
 
-
-		if (i == n - 1)
-			printf("\n");
-		else if (separator == NULL)
-			;
-		else
+		if (i < n-1 && separator != NULL)
 			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(strings);
 }
