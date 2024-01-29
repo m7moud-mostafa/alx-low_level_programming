907e1af53a1db8f2c2cd3826b522e6a6ce7dc24b
Mahmoud <mah2002moud@gmail.com>
2024-01-29 02:11:40 +0200

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,35 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates string
+ * @str: string
+ * Return: duplicated string
+ *
+ */
+
+char *_strdup(char *str)
+{
+	int len = 0;
+	int i;
+	char *dust;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+
+	dust = malloc(len + 1);
+	if (dust == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dust[i] = str[i];
+
+	return (dust);
+}
 
 /**
  * print_strings - prints strings, followed by a new line
@@ -9,20 +38,29 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list strings;
 	unsigned int i;
-	char *str;
-	va_list args;
+	char *string;
 
-	va_start(args, n);
-
+	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
-		printf("%s", str ? str : "(nil)");
-		if (i < n - 1 && separator != NULL)
+		string = _strdup(va_arg(strings, char *));
+
+		if (string == NULL)
+			printf("(nil)");
+
+		else
+			printf("%s", string);
+		free(string);
+
+
+		if (i == n - 1)
+			printf("\n");
+		else if (separator == NULL)
+			;
+		else
 			printf("%s", separator);
 	}
-
-	printf("\n");
-	va_end(args);
+	va_end(strings);
 }
