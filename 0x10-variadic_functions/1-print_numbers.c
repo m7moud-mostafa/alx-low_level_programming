fb4931d16effa147d78cf4f13f2f269c078ad02e
Mahmoud <mah2002moud@gmail.com>
2024-01-29 02:20:02 +0200

    AlxProject

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,35 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates string
+ * @str: string
+ * Return: duplicated string
+ * 
+ */
+
+char *_strdup(char *str)
+{
+	int len;
+	int i;
+	char *dust;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+
+	dust = malloc(len + 1);
+	if (dust == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dust[i] = str[i];
+
+	return (dust);
+}
 
 /**
  * print_numbers - prints numbers, followed by a new line
@@ -9,20 +38,29 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list integers;
+	va_list strings;
 	unsigned int i;
+	char *string;
 
-	va_start(integers, n);
+	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", va_arg(integers, int));
+		string = _strdup(va_arg(strings, char *));
 
-		if (separator == NULL)
-			;
-		else if (i == n - 1)
+		if (string == NULL)
+			printf ("(nil)");
+
+		else
+			printf ("%s", string);
+		free (string);
+
+
+		if (i == n - 1)
 			printf("\n");
+		else if (separator == NULL)
+			;
 		else
 			printf("%s", separator);
 	}
-	va_end(integers);
+	va_end(strings);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,34 +8,5 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	char *str, *sep = "";
-	va_list args;
 
-	va_start(args, format);
-
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%s%c", sep, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				printf("%s%s", sep, str ? str : "(nil)");
-				break;
-		}
-		sep = ", ";
-		i++;
-	}
-
-	printf("\n");
-	va_end(args);
 }
