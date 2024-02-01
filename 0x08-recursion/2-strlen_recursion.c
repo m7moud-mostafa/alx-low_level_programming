da7ffebf94d9eff5cf911ffeb64912b00a22de65
Mahmoud <mah2002moud@gmail.com>
2024-02-01 06:54:03 +0200

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
  * _strlen_recursion - calculates the length of a string
  *
  * @s: the string
- * Return: length of the string 
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
