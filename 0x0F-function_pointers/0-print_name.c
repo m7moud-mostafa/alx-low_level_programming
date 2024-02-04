723493591280d74d1789699c53dbbfec7fd0ebcf
Mahmoud <mah2002moud@gmail.com>
2024-02-05 00:11:10 +0200

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name that was passed to the function
+ *
+ * @name: the name to be printed
+ * @f: The style which the name will be printed with
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
