c0fcf476e07315efff0f0f9eae3a8360453647dd
Mahmoud <mah2002moud@gmail.com>
2024-02-07 01:43:50 +0200

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,6 +21,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	tmp->n = n;
 	tmp->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return (tmp);
+	}
+
 	ptr = *head;
 	for (i = 0; ptr->next != NULL; i++)
 	{
