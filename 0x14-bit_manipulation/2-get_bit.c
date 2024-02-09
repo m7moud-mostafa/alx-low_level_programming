5ea4a9499a124e692fc10c833670152cc7d376f2
Mahmoud <mah2002moud@gmail.com>
2024-02-09 04:40:52 +0200

    AlxProject

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to search in
+ * @index: index of the bit
+ *
+ * Return: value of the bit, or -1 if an error occurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+		return (-1);
+
+	return ((n >> index) & 1);
+}
