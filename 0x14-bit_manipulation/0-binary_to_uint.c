68bed3728ea4e3b508b9de091e0b30060f24b860
Mahmoud <mah2002moud@gmail.com>
2024-02-09 04:42:52 +0200

    AlxProject

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,8 +4,9 @@
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: string containing the binary number
  *
- * Return: the converted number, or 0 if there is one or more chars in the string
- * b that is not 0 or 1 b is NULL
+ * Return: the converted number, or 0 if there is
+ * one or more chars in the string b that is not
+ * 0 or 1 b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
