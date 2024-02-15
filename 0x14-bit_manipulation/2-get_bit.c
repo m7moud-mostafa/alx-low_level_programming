29665a5b0c069004d73a2906144d43319d249528
Mahmoud <mah2002moud@gmail.com>
2024-02-16 01:47:58 +0200

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,15 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(n) * 8))
+	while (n && index > 0)
+	{
+		n >>= 1;
+		index--;
+	}
+
+	if (n && index)
 		return (-1);
 
-	return ((n >> index) & 1);
+	return (n & 1);
+
 }
