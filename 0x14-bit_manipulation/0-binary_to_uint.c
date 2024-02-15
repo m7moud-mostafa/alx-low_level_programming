a345b6815a691b16fffd5600b3b8cadfe4f4c635
Mahmoud <mah2002moud@gmail.com>
2024-02-16 00:09:37 +0200

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * power - calculates base ^ exponent
+ *
+ * @base: the base
+ * @exponent: the exponent
+ * Return: int
+ */
+int power(int base, int exponent)
+{
+	int result = 1;
+
+	while (exponent > 0)
+	{
+		result *= base;
+		--exponent;
+	}
+	return (result);
+}
+
+/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: string containing the binary number
  *
@@ -13,16 +32,23 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 
 	if (!b)
-	return (0);
-
-	while (*b)
-	{
-	num <<= 1;
-	if (*b != '0' && *b != '1')
 		return (0);
-	if (*b == '1')
-		num += 1;
-	b++;
+
+	while (*b != '\0')
+	{
+		num <<= 1;
+		switch (*b)
+		{
+		case '1':
+			num++;
+			break;
+		case '0':
+			break;
+		default:
+			return (0);
+			break;
+		}
+		b++;
 	}
 	return (num);
 }
