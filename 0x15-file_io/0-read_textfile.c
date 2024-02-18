dbcf345a08b5dc67e6bffefed88c35875b36f138
Mahmoud <mah2002moud@gmail.com>
2024-02-18 13:38:08 +0200

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -12,35 +13,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_written;
+	int file;
+	ssize_t n_read, n_write;
 	char *buffer;
-
-	if (!filename)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
-		return (0);
-
-	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
 	{
-		free(buffer);
-		close(fd);
+		return (0);
+	}
+	if (!filename)
+	{
 		return (0);
 	}
 
-	n_written = write(STDOUT_FILENO, buffer, n_read);
-	free(buffer);
-	close(fd);
-
-	if (n_written != n_read)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
 		return (0);
+	}
 
-	return (n_written);
+	n_read = read(file, buffer, letters);
+	if (n_read == -1)
+	{
+		return (0);
+	}
+
+	n_write = write(STDOUT_FILENO, buffer, n_read);
+	free(buffer);
+	close(file);
+
+	if (n_write == -1 || n_read != n_write)
+	{
+		return (0);
+	}
+
+	return (n_write);
 }
