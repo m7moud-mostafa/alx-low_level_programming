21c4c14e81b4dd4c2f885911df3119eab1d11c18
Mahmoud <mah2002moud@gmail.com>
2024-02-18 14:18:20 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,28 +11,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t n_written;
+	int file;
+	int n_write;
+	int len = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		for (n_written = 0; text_content[n_written];)
-			n_written++;
+		while (text_content[len] != '\0')
+			len++;
 
-		if (write(fd, text_content, n_written) != n_written)
+		n_write = write(file, text_content, len);
+		if (n_write == -1 || n_write != len)
 		{
-			close(fd);
+			close(file);
 			return (-1);
 		}
-	}
 
-	close(fd);
+	}
+	close(file);
 	return (1);
 }
