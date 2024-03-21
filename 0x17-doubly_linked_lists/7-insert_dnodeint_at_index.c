0ab66f27d85ed516bb8b20135f2e2579e6534e09
Mahmoud <mah2002moud@gmail.com>
2024-03-22 00:30:06 +0200

    AlxProject

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint_at_index.c
@@ -0,0 +1,55 @@
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the head of the list
+ * @idx: index where the new node should be added, starting at 0
+ * @n: integer to be added to the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	temp = *h;
+	while (temp != NULL && count < idx - 1)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+	if (temp == NULL && idx > count)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
+}
