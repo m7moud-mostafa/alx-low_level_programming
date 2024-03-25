dbb247d65c81320a6615c59177d0d504852568cb
Mahmoud <mah2002moud@gmail.com>
2024-03-25 18:08:10 +0200

    AlxProject

---
+++ 0x18-dynamic_libraries/empty_functions.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+int _putchar(char c) {
+    // TODO: Implement the function
+}
+
+int _islower(int c) {
+    // TODO: Implement the function
+}
+
+int _isalpha(int c) {
+    // TODO: Implement the function
+}
+
+int _abs(int n) {
+    // TODO: Implement the function
+}
+
+int _isupper(int c) {
+    // TODO: Implement the function
+}
+
+int _isdigit(int c) {
+    // TODO: Implement the function
+}
+
+int _strlen(char *s) {
+    // TODO: Implement the function
+}
+
+void _puts(char *s) {
+    // TODO: Implement the function
+}
+
+char *_strcpy(char *dest, char *src) {
+    // TODO: Implement the function
+}
+
+int _atoi(char *s) {
+    // TODO: Implement the function
+}
+
+char *_strcat(char *dest, char *src) {
+    // TODO: Implement the function
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    // TODO: Implement the function
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    // TODO: Implement the function
+}
+
+int _strcmp(char *s1, char *s2) {
+    // TODO: Implement the function
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    // TODO: Implement the function
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    // TODO: Implement the function
+}
+
+char *_strchr(char *s, char c) {
+    // TODO: Implement the function
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    // TODO: Implement the function
+}
+
+char *_strpbrk(char *s, char *accept) {
+    // TODO: Implement the function
+}
+
+char *_strstr(char *haystack, char *needle) {
+    // TODO: Implement the function
+}
+
