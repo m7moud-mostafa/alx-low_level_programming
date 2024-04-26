cf1e0911cbf6d341a2553c734a252ef59b320d18
Mahmoud <mah2002moud@gmail.com>
2024-04-26 11:12:40 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
 	hash_node_t **array;
-	int i;
+	unsigned long int i;
 
 	hash = malloc(sizeof(hash_table_t));
 	if (!hash)
