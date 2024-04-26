276fa615a760c9d4c5c484e31892796e12a2112f
Mahmoud <mah2002moud@gmail.com>
2024-04-26 11:54:08 +0300

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,29 +12,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *new_node, *idx_node;
 	unsigned long int  key_idx;
 
 	if (!ht || !key || !value || !(*key))
 		return (0);
 
 	key_idx = key_index(key, ht->size);
+	/*Searches for the key and adds its value to the key*/
+	idx_node = ht->array[key_idx];
+	while (idx_node)
+	{
+		if (!strcmp(idx_node->key, key))
+		{
+			idx_node->value = strdup(value);
+			return (1);
+		}
+		idx_node = idx_node->next;
+	}
 
-	/*Create the node in some place in memory*/
-	node = malloc(sizeof(hash_node_t));
-	if (!node)
+	/*If the key is not found in the hash table*/
+
+	/*Create the new_node in some place in memory*/
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
 		return (0);
 
-	node->key = strdup(key);
-	node->value = strdup(value);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 
 	if (ht->array[key_idx] == NULL)
-		node->next = NULL;
+		new_node->next = NULL;
 	else
-		node->next = ht->array[key_idx];
+		new_node->next = ht->array[key_idx];
 
 	/*Add the node to its index*/
-	ht->array[key_idx] = node;
+	ht->array[key_idx] = new_node;
 
 	return (1);
 }
